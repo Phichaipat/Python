ff285f539376020f757ad835b2a66c4018ddeafd
Phichaipat <6529011011@cdti.ac.th>
2024-06-10 16:27:18 +0700

    first commit

---
+++ lab1.py
@@ -0,0 +1,5 @@
+print("Hi")
+A = 1
+B = 20
+o = A+B
+print("คำตอบ \n" + str(o) + " งับ")
---
+++ lab2.py
@@ -0,0 +1,6 @@
+#base = input("รับค่าฐาน: ")
+#height = input("รับสูง: ")
+base = input("รับค่าฐาน: ")
+height = input("รับสูง: ")
+area = 1/2*int(base)*int(height)
+print("คำตอบ " + str(area))
