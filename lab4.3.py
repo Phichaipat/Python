33004d16c74b9d85667ea1e4b0e1b64f35678a52
Phichaipat <6529011011@cdti.ac.th>
2024-06-24 14:44:25 +0700

    updata

---
+++ lab4.3.py
@@ -0,0 +1,9 @@
+person = {"name": "Phichaipat", "age":17, "wt":75, "h":184}
+for n in person:
+    print(n)
+print(person["name"])
+print(person["age"])
+print(person["wt"])
+print(person["h"])
+person["name"] = "Phichaipat"
+print(person["name"])
